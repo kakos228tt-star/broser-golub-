adbd8a61521320d3c70c3e08ca4da0e2c54a7ac5
kakos228tt-star <kakos228tt@gmail.com>
2026-07-28 20:54:52 +0400

    Add files via upload

---
+++ info.bat
@@ -0,0 +1,19 @@
+@echo off
+title About Pigeon Projects
+cls
+echo ************************************
+echo *       PIGEON PROJECTS            *
+echo *         about / about            *
+echo ************************************
+echo.
+echo   Repository #1 (safe-viruses)
+echo   https://github.com/softnastandoff13-oss/safe-viruses
+echo.
+echo   Repository #2 (broser-golub-)
+echo   https://github.com/kakos228tt-star/broser-golub-/tree/main
+echo.
+echo ************************************
+echo.
+echo   Press any key to exit...
+pause > nul
+exit
