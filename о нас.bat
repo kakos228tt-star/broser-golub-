36dd305d94d5bee0c3656c3b0ffcee7da4bc240d
kakos228tt-star <kakos228tt@gmail.com>
2026-07-28 20:51:37 +0400

    Update о нас.bat

--- о нас.bat
+++ о нас.bat
@@ -1,21 +1,20 @@
 @echo off
-chcp 65001 > nul
-title 🕊️ О нас — Pigeon Projects
+title Pigeon Projects - About
 cls
 
 echo ************************************
-echo *       🕊️  PIGEON PROJECTS        *
-echo *         о нас / about            *
+echo *       PIGEON PROJECTS            *
+echo *         about / about            *
 echo ************************************
 echo.
-echo   📦 Репозиторий #1 (safe-viruses)
+echo   Repository #1 (safe-viruses)
 echo   https://github.com/softnastandoff13-oss/safe-viruses
 echo.
-echo   🌐 Репозиторий #2 (broser-golub-)
+echo   Repository #2 (broser-golub-)
 echo   https://github.com/kakos228tt-star/broser-golub-/tree/main
 echo.
 echo ************************************
 echo.
-echo   Нажми любую клавишу для выхода...
+echo   Press any key to exit...
 pause > nul
 exit
